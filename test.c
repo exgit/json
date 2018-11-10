a6323cd58a1e33285610061fd11dc1ef6ba64b49
oleg.alexeev <oleg.alexeev@inbox.ru>
2018-11-10 20:21:46 +0300

    new test added

--- test.c
+++ test.c
@@ -587,10 +587,12 @@ static int SomeComplexExamples(void) {
         char str1[64], str2[64], str3[64], str4[64];
         char c1, c2, c3, c4;
         short s1, s2, s3, s4;
-        int n1, n2, n3, n4;
-        float f1, f2, f3, f4, f5;
+        int n1, n2, n3, n4, n5;
+        float f1, f2, f3, f4;
         double d1, d2, d3, d4;
     } s;
+    float epsf = 0.000001f;
+    double epsd = 0.000000001;
     const char *j;
     int res;
 
@@ -616,6 +618,76 @@ static int SomeComplexExamples(void) {
     if (s.c1 != 1 || s.c2 != 2) return 1;
     if (s.c3 != 3 || s.c4 != 4) return 1;
     if (strcmp(s.str1, "a") || strcmp(s.str2, "b")) return 1;
+
+    j= "  \
+[  \
+  100,  \
+  {  \
+    'aaa': 1000,  \
+    'bbb': 2000,  \
+    'ccc': [  \
+      10,  \
+      20,  \
+      {'ddd': 111, 'eee': 222, 'fff': 333},  \
+      30  \
+    ]  \
+  },  \
+  200,  \
+  300,  \
+  [  \
+    'abcdefg',  \
+    'bcdefgh'  \
+  ],  \
+  {  \
+    'ggg': 1.23,  \
+    'hhh': 57.77e-1,  \
+    'iii': 0.007e3  \
+  }  \
+]";
+    atr arr_obj_arr_obj[] = {
+        {"ddd", MINT, sizeof(s.s1), &s.s1},
+        {"eee", MINT, sizeof(s.s2), &s.s2},
+        {"fff", MINT, sizeof(s.s3), &s.s3}
+    };
+    elt arr_obj_arr[] = {
+        {MINT, sizeof(s.c1), &s.c1},
+        {MINT, sizeof(s.c2), &s.c2},
+        {MOBJ, 3, &arr_obj_arr_obj},
+        {MINT, sizeof(s.c3), &s.c3}
+    };
+    atr arr_obj1[] = {
+        {"aaa", MINT, sizeof(s.n1), &s.n1},
+        {"bbb", MINT, sizeof(s.n2), &s.n2},
+        {"ccc", MARR, 4, arr_obj_arr},
+    };
+    elt arr_arr2[] = {
+        {MSTR, sizeof(s.str1), &s.str1},
+        {MSTR, sizeof(s.str2), &s.str2}
+    };
+    atr arr_obj3[] = {
+        {"ggg", MFLT, sizeof(s.f1), &s.f1},
+        {"hhh", MFLT, sizeof(s.f2), &s.f2},
+        {"iii", MFLT, sizeof(s.d1), &s.d1}
+    };
+    elt arr[] = {
+        {MINT, sizeof(s.n3), &s.n3},
+        {MOBJ, 3, arr_obj1},
+        {MINT, sizeof(s.n4), &s.n4},
+        {MINT, sizeof(s.n5), &s.n5},
+        {MARR, 2, &arr_arr2},
+        {MOBJ, 3, &arr_obj3}
+    };
+    elt first = {MARR, 6, &arr};
+    memset(&s, 0, sizeof(s));
+    res = json_read(&first, j, strlen(j));
+    if (res) return 1;
+    if (s.c1 != 10 || s.c2 != 20 || s.c3 != 30) return 1;
+    if (s.s1 != 111 || s.s2 != 222 || s.s3 != 333) return 1;
+    if (s.n1 != 1000 || s.n2 != 2000 || s.n3 != 100 || s.n4 != 200 || s.n5 != 300) return 1;
+    if (strcmp(s.str1, "abcdefg") || strcmp(s.str2, "bcdefgh")) return 1;
+    if (!(1.23f - epsf <= s.f1 && s.f1 <= 1.23f + epsf)) return 1;
+    if (!(5.777f - epsf <= s.f2 && s.f2 <= 5.777f + epsf)) return 1;
+    if (!(7.0 - epsd <= s.d1 && s.d1 <= 7.0 + epsd)) return 1;
 
     return 0;
 }
